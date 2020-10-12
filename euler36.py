85dc80e6caddb2d180ff2dccc484aacd53d9baff
BruE0 <bruno7497@gmail.com>
2020-10-11 23:07:39 -0300

    Easier than expected

--- euler36.py
+++ euler36.py
@@ -5,4 +5,10 @@
     2020
 """
 
-# TO DO
+def is_palindrome(number):
+    return str(number) == str(number)[::-1]
+
+total = sum(n for n in range(1_000_000) if is_palindrome(n) and is_palindrome(f"{n:b}"))
+
+print(total)
+# 872187
